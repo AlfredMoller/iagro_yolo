2e600117f19e3ac18a1824c34dfb5b247fe48791
pc <almoller622@gmail.com>
2021-10-18 03:08:15 -0300

    Updated dropbox repo creator

--- dbx_droptest.py
+++ dbx_droptest.py
@@ -2,14 +2,9 @@ import sys
 import dropbox
 from dropbox.exceptions import ApiError, AuthError
 
-
 dbox_key = '2aPXeDN95_sAAAAAAAAAAcP47wmRizJ1bHJheJHrJJ_dwVOefc3W7lxgL0MbJPGe'
 dbx = dropbox.Dropbox(dbox_key)
 dropbox_bdir = '/test_dropbox3/'
-pc_bdir = '/detections/resize'
-pc_fname = 'detection.jpg'
-file_path = pc_bdir + pc_fname
-name_dir = 'elizabeth_images'
 
 
 def dbox_metadata():
@@ -18,40 +13,36 @@ def dbox_metadata():
         list_dir_ext.append(entry.name)
     return list_dir_ext
 
-print('listado de recursos:',dbox_metadata())
 
-
-
-def check_ext(mail):
-    if '_images' in mail:
+def check_ext(file_ext):
+    if '_images' in file_ext:
         return True
     else:
         return False
 
 
-#   Uso de la función filter para
-#   comprobar emails válidos de una lista.
-def dbx_crt_folder():
-    valid_folder_ext = filter(check_ext, dbox_metadata())
-    if name_dir in list(valid_folder_ext):
-       print("existe el repositorio")
-    else:
-       print("procedemos a crear repo")
+# Checking the extension of folder instance  exist in all metadatas
+def dbx_crt_folder(name_dir):
+    try:
+        valid_folder_ext = filter(check_ext, dbox_metadata())
+        if name_dir in list(valid_folder_ext):
+            return {"status":303, "msg" : "El repositorio existe...Intentelo con otro nombre!"}
+        else:
+            dbx.files_create_folder_v2("/" + name_dir)
+            return {"status": 200, "msg": "El repositorio fue creado exitosamente!"}
+    except ApiError as err:
+        print(err)
 
 
-
-def dbx_upload(filestr,f_name):
-    msg_upload = None
+def dbx_upload(filestr, f_name):
     try:
         push_path_folder = dropbox_bdir + f_name
         dbx.files_upload(filestr, push_path_folder, mode=dropbox.files.WriteMode("overwrite"))
     except ApiError as err:
-        if(err.error.is_path() and
-                    err.error.get_path().error.is_insufficient_space()):
-            sys.exit("ERROR: No se puede realizar procedimiento de almacenado, hay poco espacio en el repo principal")
+        if (err.error.is_path() and
+                err.error.get_path().error.is_insufficient_space()):
+            print("ERROR: No se puede realizar procedimiento de almacenado, hay poco espacio en el repo principal")
         elif err.user_message_text:
             print(err.user_message_text)
-            sys.exit()
         else:
             print(err)
-            sys.exit()
